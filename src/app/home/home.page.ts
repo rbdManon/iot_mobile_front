393db6dfc0c589315df8ed048ab72b33472e7f6e
LucasBlin <blin.lucas2@gmail.com>
2019-06-16 19:04:10 +0200

    retrieve data from api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-  
+  constructor() { }
+
+  ngAfterViewInit () {
+    console.log('test')
+      setInterval(requestData, 1000);
+
+    function requestData() {
+      var array = []
+      $.getJSON('http://localhost:3000/getAllSensor', function (data) {
+        for (var element in data) {
+          if (data[element].is_active) {
+            array.push(data[element].id)
+          }
+        }
+      }).then(function () {
+        $(".ion-col").each(function () {
+          console.log($(this))
+          if (array.includes(this.getAttribute('value'))) {
+            $(this).addClass('available')
+          } else {
+            $(this).addClass('not_available')
+          }
+        });
+      })
+    }
+  }
 }
