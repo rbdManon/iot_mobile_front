7f4873881c4cb60dddb4c2c7811bfdd79f26a5db
Manon-Rbd <rambau.manon@gmail.com>
2019-06-18 22:50:50 +0200

    create alerte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import * as $ from "jquery";
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,10 +9,48 @@ import * as $ from "jquery";
 })
 export class HomePage {
 
-  constructor() { }
+  constructor(public alertCtrl : AlertController) { }
+  public verifBook(event){
+    let id:string;
+    id =event.target.id;
+    this.alertCtrl.create({
+      header: 'Informations',
+      subHeader: 'Confirmation reservation',
+      message: "Voulez-vous reserver cette place de parking ?",
+      inputs: [
+        {
+          name: 'book',
+          type: 'text',
+          placeholder: 'Reservation',
+          value:id,
+          disabled:true
+        }],
+        buttons: [
+          {
+            text: 'Annuler',
+            role: 'cancel',
+            cssClass: 'secondary'
+          }, {
+            text: 'Réserver',
+            handler: () => {
+              this.book(id)
+            }
+          }
+        ]
+    }).then((alert) => alert.present()); 
+  }
+  public book(id){
+       $.ajax({
+        url:'http://localhost:3000/updateSensor/'+id,
+        type:'PUT',
+        contentType:'application/json',
+        data:JSON.stringify({"_id": "5cbf54182c339b42c8b95cd0","id": "A","is_active": true, "__v": 0,"is_booking": true  })
+      });
+      
 
+  }
   ngAfterViewInit () {
-    console.log('test')
+    //console.log('test')
       setInterval(requestData, 1000);
 
     function requestData() {
@@ -23,7 +63,7 @@ export class HomePage {
         }
       }).then(function () {
         $(".ion-col").each(function () {
-          console.log($(this))
+          //console.log($(this))
           if (array.includes(this.getAttribute('value'))) {
             $(this).addClass('available')
           } else {
